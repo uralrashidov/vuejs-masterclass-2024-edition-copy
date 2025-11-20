f094a6eb398ce712a3b18c874bcb81f7912d41a2
uralrashidov <uralrashidov1112@gmail.com>
2025-11-20 23:27:26 +0500

    add ref

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+  import { supabase } from '@/lib/supabaseClient';
+  import { ref } from 'vue';
 
+  const projects = ref();
+
+  (async () => {
+    const { data, error } = await supabase.from('projects').select();
+    if (error) {
+      console.error(error);
+    } else {
+      projects.value = data;
+      console.log('Projects : ', projects.value);
+    }
+  })();
 </script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
+    {{ projects }}
     <RouterLink to="/">Go to Home</RouterLink>
   </div>
 </template>
