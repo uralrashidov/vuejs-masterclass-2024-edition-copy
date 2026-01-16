2da19cec7b36743a83d0d688e56560e27ac034bc
uralrashidov <uralrashidov1112@gmail.com>
2026-01-16 22:52:23 +0500

    update

--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -14,3 +14,40 @@ export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 export const projectsQuery = supabase.from('projects').select()
 export type Projects = QueryData<typeof projectsQuery>
+
+export const projectQuery = (slug: string) =>
+  supabase
+    .from('projects')
+    .select(
+      `
+  *,
+  tasks (
+    id,
+    name,
+    status,
+    due_date
+  )
+`,
+    )
+    .eq('slug', slug)
+    .single()
+
+export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskQuery = (id: number) =>
+  supabase
+    .from('tasks')
+    .select(
+      `
+  *,
+  projects (
+    id,
+    name,
+    slug
+  )
+`,
+    )
+    .eq('id', id)
+    .single()
+
+export type Task = QueryData<ReturnType<typeof taskQuery>>
